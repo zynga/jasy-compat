b80099511adfcb8a026de5ad0147201540ccbd19
Sebastian Werner <swerner@zynga.com>
2012-03-16 16:54:52 +0100

    Updated for simplifications in newest jasy

--- jasyscript.py
+++ jasyscript.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env jasy
 
-session = Session()
-optimization = Optimization("blocks", "declarations", "variables", "privates")
-formatting = Formatting()
-
 @task("Build")
 def build():
     buildFolder = "build"
     
-    resolver = Resolver(session.getProjects())
+    resolver = Resolver()
     resolver.addClassName("compatty.Main")
     
-    asset = Asset(session, resolver.getIncludedClasses())
-    kernelClasses = storeKernel("%s/script/kernel.js" % buildFolder, session, debug=True, assets=asset.exportBuild(buildFolder=buildFolder))
+    asset = Asset(resolver.getIncludedClasses())
+    kernelClasses = storeKernel("%s/script/kernel.js" % buildFolder, debug=True, assets=asset.exportBuild(buildFolder=buildFolder))
     
     resolver.excludeClasses(kernelClasses)
     classes = Sorter(resolver).getSortedClasses()
     
-    storeCompressed("%s/script/main.js" % buildFolder, classes, formatting=formatting, optimization=optimization)
+    storeCompressed("%s/script/main.js" % buildFolder, classes)
     copyFile("source/index.html", "build/index.html")
     
     
 @task("Source")
 def source():
-    resolver = Resolver(session.getProjects())
+    resolver = Resolver()
     resolver.addClassName("compatty.Main")
 
-    asset = Asset(session, resolver.getIncludedClasses())
-    kernelClasses = storeKernel("source/script/kernel.js", session, debug=True, assets=asset.exportSource())
+    asset = Asset(resolver.getIncludedClasses())
+    kernelClasses = storeKernel("source/script/kernel.js", debug=True, assets=asset.exportSource())
 
     resolver.excludeClasses(kernelClasses)
     classes = Sorter(resolver).getSortedClasses()
 
-    storeSourceLoader("source/script/main.js", classes, session)
+    storeSourceLoader("source/script/main.js", classes)
