4a6726644ee65e2875f06d4623fe93b7465ef07a
Sebastian Werner <swerner@zynga.com>
2012-09-24 15:52:22 +0200

    Switched to YAML format.

--- jasyscript.py
+++ jasyscript.py
@@ -25,9 +25,9 @@ def build():
     outputManager = OutputManager(session, assetManager)
 
     outputManager.storeKernel("$prefix/script/kernel.js", debug=True)
-    outputManager.deployAssets(["compatty.Main"])
+    outputManager.deployAssets(["jasycompat.Main"])
 
-    classes = Resolver(session).addClassName("compatty.Main").getSortedClasses()
+    classes = Resolver(session).addClassName("jasycompat.Main").getSortedClasses()
     outputManager.storeCompressed(classes, "$prefix/script/main.js")
     
     FileManager(session).updateFile("source/index.html", "$prefix/index.html")
@@ -42,5 +42,5 @@ def source():
 
     outputManager.storeKernel("$prefix/script/kernel.js", debug=True)
 
-    classes = Resolver(session).addClassName("compatty.Main").getSortedClasses()
+    classes = Resolver(session).addClassName("jasycompat.Main").getSortedClasses()
     outputManager.storeLoader(classes, "$prefix/script/main.js")
